45599466b0b0b2d9016fc1127382b42e2f9918ce
Yann Dupont <dupont-y@duponty-HP-EliteBook-2570p.(none)>
2014-04-18 11:34:18 +0200

    airport work...

--- storapelago-bootstrap.py
+++ storapelago-bootstrap.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python
+## Not forcing python 3.4 right now
 
 from yapsy.PluginManager import PluginManager
 
 # Build the manager
 simplePluginManager = PluginManager()
 # Tell it the default place(s) where to find plugins
-simplePluginManager.setPluginPlaces(["../plugins"])
+simplePluginManager.setPluginPlaces(["plugins"])
 # Load all plugins
+#print (simplePluginManager.locatePlugins ())
 simplePluginManager.collectPlugins()
 
 # Activate all loaded plugins
+
+simplePluginManager.getAllPlugins()
+
 for pluginInfo in simplePluginManager.getAllPlugins():
-   print ("1")
    simplePluginManager.activatePluginByName(pluginInfo.name)
+   pluginInfo.plugin_object.print_name()
+   print pluginInfo
    
